2d5fa8d02db757b50cad674ad0c5e31d7aedc3a6
Huabei You <huabeiyou@gmail.com>
2023-03-25 12:30:05 -0400

    update

--- md2pdf.sh
+++ md2pdf.sh
@@ -1,22 +1,57 @@
 #!/bin/bash
 
+usage() {
+	echo "Usage: $0 input_file.md [-l|--lang language]"
+	exit 1
+}
+
+check_input_file() {
+	echo $1
+	if [[ "$1" != *.md ]]; then
+		echo "Input file must end with .md"
+		usage
+		exit 1
+	fi
+}
+
+# --template eisvogel
+process_args() {
+	while [[ $# -gt 0 ]]; do
+		key="$1"
+		case $key in
+		-l | --lang)
+			lang="$2"
+			shift 2
+			;;
+		*)
+			shift
+			;;
+		esac
+	done
+
+	# Set default value for lang if not specified
+	if [ -z "$lang" ]; then
+		lang="en"
+	fi
+}
+
+construct_cmd() {
+	if [ "$lang" = "cn" ] || [ "$lang" = "chinese" ]; then
+		cmd="pandoc --from markdown --pdf-engine xelatex --listings -V geometry:margin=1in -V CJKmainfont=STSong -o $output $input"
+	else
+		cmd="pandoc --from markdown --pdf-engine xelatex --listings -V geometry:margin=1in -o $output $input"
+	fi
+}
+
 if [ $# -eq 0 ]; then
-	echo "No input file specified"
-	exit 1
+	usage
 fi
 
-if [[ "$1" != *.md ]]; then
-	echo "Input file must end with .md"
-	exit 1
-fi
+input="$1"
+check_input_file "$input"
+output="${input%.md}.pdf"
 
-input=$1
-output=${input%.md}.pdf
+process_args "$@"
+construct_cmd
 
-pandoc \
-	--from markdown \
-	--pdf-engine xelatex \
-	--listings \
-	-V geometry:margin=1in \
-	-V CJKmainfont="STSong" \
-	-o "$output" "$input" # --template eisvogel \
+eval "$cmd"
